513c812d6b3a64fe6c99d3e6aa59c643ee095c8f
harrietmjones <harrietmjones@eaton.com>
2026-07-15 15:59:59 +0100

    Refactor smoothie.py for ingredient selection logic

--- smoothie.py
+++ smoothie.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
 import requests
+from snowflake.snowpark.functions import col
  
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -23,14 +23,13 @@ ingredients_list = st.multiselect(
     , my_dataframe.select(col('FRUIT_NAME'))
     , max_selections=5
     )
-if ingredients_list:
- 
-    ingredients_string = ''
 
+if ingredients_list:
+ ingredients_string =''
  for fruit_chosen in ingredients_list:
-     ingredients_string += fruit_chosen + ' '
-     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-     sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width= True)
+  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+  sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width= True)
+
 
     #st.write(ingredients_string)
  
