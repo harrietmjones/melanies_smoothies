a5ec02e7257969e90d0d4c0881f81d65eb0a206a
harrietmjones <harrietmjones@eaton.com>
2026-07-15 12:25:36 +0100

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- smoothie.py
+++ smoothie.py
@@ -28,7 +28,7 @@ if ingredients_list:
  
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
- 
+
     #st.write(ingredients_string)
  
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
