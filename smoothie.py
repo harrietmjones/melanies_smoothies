b1b2e0f8faec13a0653406c0f1fc5c803ec2c496
harrietmjones <harrietmjones@eaton.com>
2026-07-16 09:39:32 +0100

    Fix indentation of success message in smoothie.py

--- smoothie.py
+++ smoothie.py
@@ -47,4 +47,4 @@ values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 time_to_insert = st.button('Submit Order')
 if time_to_insert:
    session.sql(my_insert_stmt).collect()
- st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")  
+  st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
